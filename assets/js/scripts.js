5274828ff614741f21e7a24c4af86a3074621dbd
Danilo Blum <dnl.blm@gmail.com>
2023-08-31 10:45:32 +0300

    organizando pastas

---
+++ assets/js/scripts.js
@@ -0,0 +1,114 @@
+// Stick Header
+
+window.addEventListener('scroll', function () {
+	const header = document.querySelector('.header');
+	const titleHeight = document.querySelector('.header').scrollHeight;
+
+	if (window.scrollY > 150) {
+		header.classList.add('header--sticky');
+	} else {
+		header.classList.remove('header--sticky');
+	}
+});
+
+// (function () {
+// 	// Popover
+// 	$(function () {
+// 		$('[data-toggle="popover"]').popover(options);
+// 	});
+
+// 	// Tooltip
+// 	$(function () {
+// 		$('[data-toggle="tooltip"]').tooltip(options);
+// 	});
+// })();
+
+// Popover
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+	return new bootstrap.Popover(popoverTriggerEl);
+});
+
+// Botão de copiar podcast
+
+const copyButton = document.querySelectorAll('.copy-to-clip');
+
+copyButton.forEach(btn => {
+	btn.addEventListener('click', () => {
+		copyToClipboard(btn);
+		// tooltipShow(btn);
+
+		tooltipFeedback(btn);
+	});
+});
+
+function copyToClipboard(e) {
+	const textToCopy = e.getAttribute('data-link');
+	const textarea = document.createElement('textarea');
+	textarea.setAttribute('readonly', '');
+	textarea.style.position = 'absolute';
+	textarea.value = textToCopy;
+	document.body.appendChild(textarea);
+	textarea.select();
+	document.execCommand('copy');
+	document.body.removeChild(textarea);
+}
+function tooltipFeedback(b) {
+	let feedback = $('[data-toggle="tooltip"]');
+
+	// feedback.tooltip('show');
+
+	b.addEventListener('mouseout', () => {
+		feedback.tooltip('hide');
+	});
+}
+
+// Lightbox (insert the class "lightbox" into <figure>)
+
+const imageToLightbox = document.querySelectorAll('.lightbox');
+
+imageToLightbox.forEach(image => {
+	image.addEventListener('click', () => {
+		if (!image.classList.contains('lightbox--show')) {
+			const getImage = image.querySelector('img');
+			const getImageSrc = getImage.getAttribute('src');
+			const imageLightbox = document.createElement('div');
+
+			imageLightbox.classList.add('lightbox__image');
+
+			document.body.appendChild(imageLightbox);
+			imageLightbox.innerHTML = `<img src="${getImageSrc}"/>`;
+			console.log(getImageSrc);
+
+			image.classList.add('lightbox--show');
+
+			document.body.style.overflow = 'hidden';
+			document.body.style.userSelect = 'none';
+
+			closeLightbox(imageLightbox);
+		}
+
+		function closeLightbox(e) {
+			const lightboxOpen = document.querySelector('.lightbox__image');
+			e.addEventListener('click', () => {
+				document.body.removeChild(e);
+				image.classList.remove('lightbox--show');
+				document.body.style.overflow = 'auto';
+				document.body.style.userSelect = 'auto';
+			});
+		}
+	});
+});
+
+// Boxes - inserir o título de acordo com o atributo
+
+const boxes = document.querySelectorAll('.box');
+
+boxes.forEach(box => {
+	const boxAttribute = box.getAttribute('data-box');
+	console.log(boxAttribute);
+
+	const boxLabel = box.querySelector('.label');
+
+	boxLabel.innerHTML = boxAttribute;
+});
